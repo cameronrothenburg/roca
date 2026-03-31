9164477e630daadd676377c4a18eff52447afe6f
Cameron-Lee Rothenburg <cam.hind98@gmail.com>
2026-03-31 16:55:28 +0100

    Remove build artifacts from tests/stdlib/

--- tests/stdlib/out/test_crypto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function test_uuid(): number;
-export declare function test_sha256_type(input: string): boolean;
-export declare function test_sha512_type(input: string): boolean;
--- tests/stdlib/out/test_url.d.ts
+++
@@ -1,9 +0,0 @@
-export interface RocaError {
-  name: string;
-  message: string;
-}
-export type RocaResult<T> = { value: T; err: null } | { value: null; err: RocaError };
-
-export declare function test_parse_valid(raw: string): RocaResult<string>;
-export declare function test_parse_invalid(raw: string): RocaResult<string>;
-export declare function test_parse_fallback(raw: string): string;
