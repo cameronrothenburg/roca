2861cb819769bab47d204c76f73331e6cb07507f
Cameron-Lee Rothenburg <cam.hind98@gmail.com>
2026-04-01 07:45:19 +0100

    ci(cli): enforce conventional commits via commitlint + husky
    Closes #44

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+export default {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "scope-enum": [2, "always", [
+            "compiler", "runtime", "native", "checker", "emitter",
+            "cli", "spec", "ci", "js", "deps",
+        ]],
+        "scope-empty": [1, "never"],
+    },
+};
